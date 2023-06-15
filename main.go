b5b4762030ac669a52a1045b30fd06e2b3ed65fd
regent-x <spiritofdamola@gmail.com>
2023-06-15 15:05:06 +0100

    total overhaul

--- main.go
+++ main.go
@@ -2,17 +2,41 @@ package main
 
 import (
     "log"
+    "time"
+    "os"
 
-    "github.com/regent-x/tele-bot/pkg/bot"
+    telebot "gopkg.in/telebot.v3"
 )
 
 func main(){
-    var bot = NewBot()
+    
+    perf := telebot.Settings{
+        Token: os.Getenv("TELEGRAM_TOKEN"),
+        Poller: &telebot.LongPoller{
+            Timeout: 3 * time.Second,
+        },
+    }
 
-    RegentBot(bot)
+    bot, err := telebot.NewBot(perf)
+    if err != nil {
+        log.Println(err)
+        os.Exit(1)
+    }
 
+    bot.Handle("/start", func(c telebot.Context)error{
+        c.Send("Yo, how r u doin?")
+        return nil
+    })
+    bot.Handle("/commands", func(c telebot.Context)error{
+        c.Send(`Here are the list of commands
+        /Greetings
+        /time
+        /date
+        /news
+        /others`)
+        return nil
+    })
 
-    log.Println("Regent is in the house")
-
+    log.Println("It has begun")
     bot.Start()
 }
